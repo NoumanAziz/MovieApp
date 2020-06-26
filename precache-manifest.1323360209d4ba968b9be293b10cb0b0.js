3bd7d9a87e097a84e14b0f8d3315ea12cb8b1a89
Nouman Aziz <m.nouman.ch@gmail.com>
2020-06-26 12:03:15 +0500

    Updates

---
+++ precache-manifest.1323360209d4ba968b9be293b10cb0b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e519843d131d68dc1da34fd52a04a4bf",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "d3fca356ebc2e3e4c7eb",
+    "url": "/MovieApp/static/css/2.e396d977.chunk.css"
+  },
+  {
+    "revision": "b25a3c417565eebc9fb7",
+    "url": "/MovieApp/static/css/main.4a92fc47.chunk.css"
+  },
+  {
+    "revision": "d3fca356ebc2e3e4c7eb",
+    "url": "/MovieApp/static/js/2.899c8de3.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/MovieApp/static/js/2.899c8de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25a3c417565eebc9fb7",
+    "url": "/MovieApp/static/js/main.d1e42103.chunk.js"
+  },
+  {
+    "revision": "cba4b7f13092147a04b2",
+    "url": "/MovieApp/static/js/runtime-main.72039ad9.js"
+  }
+]);
